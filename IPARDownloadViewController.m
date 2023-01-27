e95f9fc0adacb5fb3733eb74d8e0711c99a227e1
0xkuj <kujmank@gmail.com>
2023-01-27 18:18:27 +0200

    show downloaded apps

--- IPARDownloadViewController.m
+++ IPARDownloadViewController.m
@@ -2,37 +2,22 @@
 #import "IPARUtils.h"
 
 @interface IPARDownloadViewController ()
-@property (nonatomic, strong) NSMutableArray *objects;
-@property (nonatomic, strong) NSMutableArray *searchResults;
+@property (nonatomic, strong) NSMutableArray *appsBeingDownloaded;
+@property (nonatomic, strong) NSMutableArray *existingApps;
 @end
 
 //IMPLEMENT SELECT COUNTRY!
 @implementation IPARDownloadViewController
 - (void)loadView {
     [super loadView];
-    // _searchResults = [NSMutableArray array];
-    // _linesStandardOutput = [NSMutableArray array];
-    // _linesErrorOutput = [NSMutableArray array];
-    //self.title = @"IPA Ranger - Download";
+    //in case i want to add percatnage to downloaded app.. but probably we will do it in nice UI and not in shit cell. after done, reload table.
+    _appsBeingDownloaded = [NSMutableArray array];
+    _existingApps = [NSMutableArray array];
+    [self populateTableWithExistingApps];
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
     [self _setUpNavigationBar2];
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
-    // UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:self];
-    // self.viewControllers = @[navigationController];
-    // navigationController.title = @"Search";
-    // UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    
-    // // Create the second view controller
-    // IPARDownloadViewController *secondViewController = [[IPARDownloadViewController alloc] init];
-    // secondViewController.title = @"Download";
-    
-    // // Add the view controllers to the tab bar controller
-    // tabBarController.viewControllers = @[navigationController, secondViewController];
-
-    // // Add the tab bar controller as the root view controller
-    // UIWindow *window = [[UIApplication sharedApplication].windows firstObject];
-    // window.rootViewController = tabBarController;
 }
 
 - (void)_setUpNavigationBar2
@@ -65,6 +50,40 @@
     UIBarButtonItem *optionsButton = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"ellipsis"] menu:menu];
     UIBarButtonItem *downloadButton = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"square.and.arrow.down.on.square.fill"] style:UIBarButtonItemStylePlain target:self action:@selector(addButtonTapped:)];
     self.navigationItem.rightBarButtonItems = @[optionsButton, downloadButton];
+}
+
+- (void)populateTableWithExistingApps {
+    NSLog(@"omriku will try to read directory content.. ");
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error = nil;
+
+    // Get an array of all files and directories in the specified directory
+    NSArray *files = [fileManager contentsOfDirectoryAtPath:IPARANGER_DOCUMENTS_LIBRARY error:&error];
+    if (error) {
+        NSLog(@"omriku Error getting contents of directory: %@", error);
+        return;
+    }
+
+    // Filter the array to only include "ipa" files
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"self ENDSWITH '.ipa'"];
+    NSArray *ipaFiles = [files filteredArrayUsingPredicate:predicate];
+
+    // Get the size of each "ipa" file
+    [self.existingApps removeAllObjects];
+    NSMutableArray *ipaFileInfos = [NSMutableArray array];
+    for (NSString *fileName in ipaFiles) {
+        NSLog(@"omriku checking file: %@", fileName);
+        NSString *filePath = [IPARANGER_DOCUMENTS_LIBRARY stringByAppendingPathComponent:fileName];
+        NSDictionary *attributes = [fileManager attributesOfItemAtPath:filePath error:&error];
+        if (error) {
+            NSLog(@"omriku Error getting attributes of file at path: %@", filePath);
+            continue;
+        }
+        NSNumber *fileSize = [attributes objectForKey:NSFileSize];
+        [self.existingApps addObject:@{@"name": fileName, @"size": fileSize}];
+    }
+    [self.tableView reloadData];
+    // Now you can use the `ipaFileInfos` array to populate your table view.
 }
 
 - (void)addButtonTapped:(id)sender {
@@ -100,7 +119,8 @@
     // if (self.searchResults.count > 0) {
     //     return self.searchResults.count;
     // }
-    return 0;
+    return self.existingApps.count + self.appsBeingDownloaded.count;
+   //return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -110,7 +130,7 @@
 		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
 	}
 
-    //cell.textLabel.text = self.searchResults[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"appname: %@ size: %@", self.existingApps[indexPath.row][@"name"], self.existingApps[indexPath.row][@"size"]]; 
 	return cell;
 }
 
@@ -126,48 +146,54 @@
 }
 
  -(void)testing:(NSString *)command {
-//     NSLog(@"omriku in testing, command %@", command);
-//     NSTask *task = [[NSTask alloc] init];
-//     NSMutableArray *args = [NSMutableArray array];
-//     [args addObject:@"-c"];
-//     [args addObject:command];
-//     [task setLaunchPath:@"/bin/sh"];
-//     [task setArguments:args];
-//     // Create a pipe for the task's standard output
-//     NSPipe *pipe = [NSPipe pipe];
-//     task.standardOutput = pipe;
+    NSLog(@"omriku in testing, command %@", command);
+    NSTask *task = [[NSTask alloc] init];
+    NSMutableArray *args = [NSMutableArray array];
+    [args addObject:@"-c"];
+    [args addObject:command];
+    [task setLaunchPath:@"/bin/sh"];
+    [task setArguments:args];
+    // Create a pipe for the task's standard output
+    NSPipe *pipe = [NSPipe pipe];
+    task.standardOutput = pipe;
 
-//     [[pipe fileHandleForReading] waitForDataInBackgroundAndNotify];
-//     NSLog(@"omriku sigining to notif..");
-//     // Register for notifications when new data is available to read
-//     [[NSNotificationCenter defaultCenter] addObserver:self
-//                                             selector:@selector(receivedData:)
-//                                                 name:NSFileHandleDataAvailableNotification
-//                                             object:[pipe fileHandleForReading]];
+    [[pipe fileHandleForReading] waitForDataInBackgroundAndNotify];
+    NSLog(@"omriku sigining to notif..");
+    // Register for notifications when new data is available to read
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                            selector:@selector(receivedData:)
+                                                name:NSFileHandleDataAvailableNotification
+                                            object:[pipe fileHandleForReading]];
 
-//     // Start the task
-//     [task launch];
+    // Start the task
+    [task launch];
  }
 
-// - (void)receivedData:(NSNotification *)notification {
-//     // Read the data from the pipe
-//     NSData *data = [[notification object] availableData];
+- (void)receivedData:(NSNotification *)notification {
+    // Read the data from the pipe
+    NSData *data = [[notification object] availableData];
 
-//     // Convert the data to a string
-//     NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-//     NSLog(@"omriku Output: %@", output);
+    // Convert the data to a string
+    NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    //NSLog(@"omriku Output: %@", output);
 
-//     // Check the percentage
-//     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\[(\\d+)%\\]" options:0 error:nil];
-//     NSArray *matches = [regex matchesInString:output options:0 range:NSMakeRange(0, output.length)];
-//     for (NSTextCheckingResult *match in matches) {
-//         // Extract the percentage from the match
-//         NSString *percentage = [output substringWithRange:[match rangeAtIndex:1]];
-//         NSLog(@"omriku Percentage: %@%%", percentage);
-//     }
+    // Check the percentage
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\[(\\d+)%\\]" options:0 error:nil];
+    NSArray *matches = [regex matchesInString:output options:0 range:NSMakeRange(0, output.length)];
+    for (NSTextCheckingResult *match in matches) {
+        // Extract the percentage from the match
+        NSString *percentage = [output substringWithRange:[match rangeAtIndex:1]];
+        NSLog(@"omriku Percentage: %@%%", percentage);
+        if ([percentage containsString:@"100"]) {
+            NSLog(@"omriku have 100!");
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5.0 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                [self populateTableWithExistingApps];
+            });
+        }
+    }
 
-//     // Register for notifications again
-//     [[notification object] waitForDataInBackgroundAndNotify];
-// }
+    // Register for notifications again
+    [[notification object] waitForDataInBackgroundAndNotify];
+}
 
 @end
